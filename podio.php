5c43bdf3b5c730c3bc8ae692bc5dca4e5c723350
Domabo <admin@domabo.com>
2014-06-20 22:52:07 -0500

    updated podio

--- podio.php
+++ podio.php
@@ -1297,7 +1297,7 @@ public static function export_feed_toPodio($entry, $form, $feed, $api)
     {
         
 
-       if (startsWith($strtolower(var_tag), 'facebook'))
+       if (self::startsWith($strtolower(var_tag), 'facebook'))
             $contact_facebook = rgar($entry, $field_id);
 
        switch(strtolower($field_id))
