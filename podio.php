b78ca01e3e17208e59e5a5fcb52f981083932564
Domabo <admin@domabo.com>
2014-06-21 09:25:46 -0500

    updated podio

--- podio.php
+++ podio.php
@@ -1312,7 +1312,7 @@ public static function export_feed_toPodio($entry, $form, $feed, $api)
      echo strpos(strtolower($var_tag), "facebook");
     {
         
-       if ( strpos(strtolower($var_tag), "facebook") != 0)
+       if ( strpos(strtolower($var_tag), "facebook") != false)
     {       $contact_facebook = rgar($entry, $field_id);
             echo $contact_facebook . " " . $var_tag;
       }
