c1d901d538b455a74a10bb76285efe29a48dd28e
Domabo <admin@domabo.com>
2014-06-21 12:41:48 -0500

    updated podio

--- podio.php
+++ podio.php
@@ -1316,7 +1316,7 @@ public static function export_feed_toPodio($entry, $form, $feed, $api)
     
         if (RGFormsModel::get_input_type($field)=="checkbox")
         {  
-           $inputs = $field["choices"][0];
+           $inputs = $field["inputs"][0];
 
 
             print_r($inputs );
