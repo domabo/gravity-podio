0046e1780a241b98c501828b277b7175ac67fa61
Domabo <admin@domabo.com>
2014-06-21 09:23:25 -0500

    updated podio

--- podio.php
+++ podio.php
@@ -1308,7 +1308,7 @@ public static function export_feed_toPodio($entry, $form, $feed, $api)
 
     $merge_vars = array();
     foreach($feed["meta"]["field_map"] as $var_tag => $field_id)
-     echo "<br>".$var_tag . ":" . $field_id ." , ";
+     echo "<br>". strtolower($var_tag) . ":" . $field_id ." , ";
      echo strpos(strtolower($var_tag), "facebook");
     {
         
