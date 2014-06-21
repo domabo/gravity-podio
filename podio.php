0f3359494231369efa871d7f621549d45ce33175
Domabo <admin@domabo.com>
2014-06-21 10:38:14 -0500

    updated podio

--- podio.php
+++ podio.php
@@ -1310,13 +1310,9 @@ public static function export_feed_toPodio($entry, $form, $feed, $api)
     $merge_vars = array();
     foreach($feed["meta"]["field_map"] as $var_tag => $field_id)
     {
-     echo "<br>". strtolower($var_tag) . ":" . $field_id ." , ";
-     echo strpos($var_tag, 'facebook');
-    
        if ( strpos(strtolower($var_tag), "facebook") !== false)
       { $contact_facebook = rgar($entry, $field_id);
-            echo "FOUND FACEBOOK " . $contact_facebook;
-      }
+       }
 
        switch(strtolower($field_id))
         {
@@ -1397,8 +1393,7 @@ public static function export_feed_toPodio($entry, $form, $feed, $api)
             {
                 $first =  $existingContacts[0];
                 $ep_profile_id = $first->profile_id;
-                echo "<br>FOUND " . $profile_id;
-
+            
                 PodioContact::update( $ep_profile_id, $contact_fields );
 
         } else
